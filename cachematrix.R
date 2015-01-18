44222bfca20a4087fd36785aa1597239ebc1ff94
wei-ann-github <wei-ann.lim@outlook.com>
2015-01-18 13:21:50 +0800

    Comments to .R added

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        # A modification of the makeVector function provided
+        # in the assignment instructions.
+        # matrix() replaces in numeric() is the function definition
+        # solve replaces mean
+        # setinverse replaces setmean
+        # getinverse replaces getmean
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setinverse <- function(cacheSolve) m <<- cacheSolve
+        setinverse <- function(solve) m <<- solve
         getinverse <- function() m
         list(set = set, get = get,
              setinverse = setinverse,
@@ -22,5 +28,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        solve(x)
+        # A modification of the cacheMean function provided 
+        # in the assignment instructions.
+        # getinverse replaces getmean
+        # solve replaces mean to get the inverse of x
+        # setinverse replaces setmean
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
