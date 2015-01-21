490fd57b0475456b64f8b1fb36e067d43b8c9add
wei-ann-github <wei-ann.lim@outlook.com>
2015-01-22 00:34:49 +0800

    more comments added

--- cachematrix.R
+++ cachematrix.R
@@ -3,43 +3,64 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-        # A modification of the makeVector function provided
-        # in the assignment instructions.
-        # matrix() replaces in numeric() is the function definition
-        # solve replaces mean
-        # setinverse replaces setmean
-        # getinverse replaces getmean
-        m <- NULL
+makeMat <- function(x = matrix()) {
+        inv <- NULL
+        
+        # This set function can actually be omitted since
+        # there is no actual use for it.
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inv <<- NULL
         }
+        
+        # Calling $get returns the matrix, x.
+        # This x is set lexically when makeVector(x) was first called
         get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
+        
+        # The inverse of x is set into $setinv via cacheinverse()
+        # by manually passing inverse into inv
+        setinv <- function(inverse) inv <<- inverse
+        
+        # The inverse of the matrix x is stored when
+        # cacheinverse is called
+        getinv <- function() inv
         list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        # A modification of the cacheMean function provided 
-        # in the assignment instructions.
-        # getinverse replaces getmean
-        # solve replaces mean to get the inverse of x
-        # setinverse replaces setmean
-        m <- x$getinverse()
-        if(!is.null(m)) {
+# The following function calculates the inverse of the special "matrix" 
+# created with the above makeMat() function. 
+# e.g. how cachemean can be used: 
+#       special.matrix <- makeMat(x) # where x is the matrix
+#       cacheinverse(special.matrix)
+# so x == special.matrix is passed into cachemean()
+# and special.matrix is a list consisting of set, get, setinv and getinv
+cacheinverse <- function(x, ...) {
+        
+        # pull out the inverse of x then....(cont. next comment)
+        inv <- x$getinv()
+        
+        # (cont. from last comment) check whether the inverse exist.
+        # if (inverse is not non-existant) ...
+        # i.e. it exists... (cont. next comment)
+        if(!is.null(inv)) {
+                
+                # (cont. from last comment) the inverse of x is returned.
                 message("getting cached data")
-                return(m)
+                return(inv) 
+                # and the function is broken out of
+                # skipping the rest of the cacheinverse function
         }
+        
+        # In the absence of inverse, we retrive the matrix and solve it,
+        # storing the result temporarily in inv
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        inv <- solve(data, ...)
+        
+        # since x is a special.matrix consisting of 
+        # set, get, setinv and getinv,
+        # x$setinv(inv) can be used to save inv, the inverse of x
+        x$setinv(inv)
+        inv
 }
