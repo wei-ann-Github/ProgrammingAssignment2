97a388004d0221af7a9ce92d260528706b4cec4a
wei-ann-github <wei-ann.lim@outlook.com>
2015-01-24 15:39:59 +0800

    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,61 +6,72 @@
 makeMat <- function(x = matrix()) {
         inv <- NULL
         
-        # This set function can actually be omitted since
-        # there is no actual use for it.
+        # This "set" function can be used in 2 ways.
+        # 1st:  mat.object <- makeMat(The.square.matrix)
+        # # The argument is directly passed into x
+        # OR
+        # 2nd:  mat.object <- makeMat()
+        #       mat.object$set(The.square.matrix)
+        # # y <- The.square.matrix, then y will be passed into x
+        #
+        # Either 1st or 2nd method, mat.object$get() will return the matrix
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
         
-        # Calling $get returns the matrix, x.
-        # This x is set lexically when makeVector(x) was first called
+        # Calling mat.object$get returns the matrix, x.
+        # This x is set when the set function above is executed.
         get <- function() x
         
-        # The inverse of x is set into $setinv via cacheinverse()
-        # by manually passing inverse into inv
+        # The inverse of x is stored automatically 
+        # into $setinv via cacheinverse(mat.object)
         setinv <- function(inverse) inv <<- inverse
         
-        # The inverse of the matrix x is stored when
-        # cacheinverse is called
+        # To retrieve the inverse of a matrix.
+        # $getinv is used in cacheinverse() as well.
         getinv <- function() inv
+        
+        # A list of what mat.object contains
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
-# The following function calculates the inverse of the special "matrix" 
-# created with the above makeMat() function. 
-# e.g. how cachemean can be used: 
-#       special.matrix <- makeMat(x) # where x is the matrix
-#       cacheinverse(special.matrix)
-# so x == special.matrix is passed into cachemean()
-# and special.matrix is a list consisting of set, get, setinv and getinv
+# The following function calculates the inverse of the mat.object
+# created with the above makeMat() function.
+# e.g. cacheinverse(mat.object)
+# returns the inverse of the matrix in mat.object
 cacheinverse <- function(x, ...) {
         
-        # pull out the inverse of x then....(cont. next comment)
+        # Pulls out the inverse of x then....(cont. next comment)
         inv <- x$getinv()
         
         # (cont. from last comment) check whether the inverse exist.
-        # if (inverse is not non-existant) ...
+        # (!is.null(inv)) == if (inverse is not non-existant) ...
         # i.e. it exists... (cont. next comment)
         if(!is.null(inv)) {
                 
                 # (cont. from last comment) the inverse of x is returned.
                 message("getting cached data")
+                
+                # the inverse is returned on the console
+                # and the function is broken out of,
+                # skipping the rest of cacheinverse()
                 return(inv) 
-                # and the function is broken out of
-                # skipping the rest of the cacheinverse function
         }
         
-        # In the absence of inverse, we retrive the matrix and solve it,
+        # In the absence of inverse, we retrive the matrix and 
+        # calculate the inverse using solve(),
         # storing the result temporarily in inv
         data <- x$get()
         inv <- solve(data, ...)
         
-        # since x is a special.matrix consisting of 
+        # since x is a mat.object consisting of 
         # set, get, setinv and getinv,
-        # x$setinv(inv) can be used to save inv, the inverse of x
+        # calling x$setinv(inv) saves the inverse.
         x$setinv(inv)
+        
+        # The inverse is returned
         inv
 }
